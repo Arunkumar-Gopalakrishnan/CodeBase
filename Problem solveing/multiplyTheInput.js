c9cf3c5fa11733101404024d48617235bed4d8f1
Arunkumar-Gopalakrishnan <arunkumarrgva@gmail.com>
2024-12-15 19:55:55 +0530

    MultiplyThe array and show as new array

---
+++ Problem solveing/multiplyTheInput.js
@@ -0,0 +1,9 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+function repeatStringAndAddItem(arr){
+    let arrayOfNumbers = arr.map(item => item*2);
+    
+    return arrayOfNumbers;
+}
+console.log(repeatStringAndAddItem([2,4,6,8]));
